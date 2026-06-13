bcc4e4ead15e97c9254873f11596790a738484bc
Aayush Bagul <135832017+aayushbagul@users.noreply.github.com>
2026-06-13 16:51:53 +0530

    Implement CORS middleware for API
    Added CORS middleware to allow requests from specified origins.

--- backend/routes/main.py
+++ backend/routes/main.py
@@ -2,8 +2,22 @@ from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 from backend.tortoise_models.config import DATABASE_URL
 from backend.routes.signup import router as signup_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(signup_router, prefix="/api")
 
